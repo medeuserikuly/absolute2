13dbdadd7326fec7f2b7333705415d4e10e8dc30
Medeu <medeuserikuly@gmail.com>
2023-09-27 10:12:34 +0600

    add accordion img

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -5,15 +5,13 @@ const accordion = (triggersSelector) => {
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function () {
             this.classList.toggle("active");
-            var panel = this.nextElementSibling;
+            let panel = this.nextElementSibling;
             if (panel.style.maxHeight) {
                 panel.style.paddingBottom = null;
                 panel.style.maxHeight = null;
-                // panel.style.marginTop = null;
 
             } else {
                 panel.style.paddingBottom = 10 + "px";
-                // panel.style.marginTop = 10 + "px";
                 panel.style.maxHeight = panel.scrollHeight + "px";
             }
         });
